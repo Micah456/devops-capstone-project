1c0badfc25e3186ca268fadcb924eec6e9bf5dce
Micah456 <michellecchuku@gmail.com>
2023-07-06 11:16:42 -0400

    Fix according to flake8

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,8 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -75,11 +77,9 @@ def list_accounts():
     return jsonify(account_list), status.HTTP_200_OK
 
 
-
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 # ... place you code here to READ an account ...
 @app.route("/accounts/<account_id>", methods=["GET"])
 def read_account(account_id):
@@ -94,11 +94,9 @@ def read_account(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<account_id>", methods=["PUT"])
 def update_account(account_id):
@@ -116,11 +114,9 @@ def update_account(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<account_id>", methods=["DELETE"])
 def delete_account(account_id):
